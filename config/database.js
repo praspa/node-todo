8b43ba97eba40bae93a30f1384335ba736925210
Patrick Raspante <praspant@praspant.laptop>
2019-09-03 21:14:11 -0500

    removed node modules and updated db to vars

--- config/database.js
+++ config/database.js
@@ -1,4 +1,12 @@
+const TODO_DB_SVC = process.env.DATABASE_SERVICE_NAME;
+const TODO_DB_USER = process.env.MONGODB_USER;
+const TODO_DB_PWD = process.env.MONGODB_PASSWORD;
+const TODO_DB = process.env.MONGODB_DATABASE;
+
+const TODO_CONN_STR = 'monogodb://' + TODO_DB_USER + ':' + TODO_DB_PWD + '@' + 'TODO_DB_SVC' + ':27017/' + TODO_DB;
+
+
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
-    localUrl: 'mongodb://localhost/meanstacktutorials'
+    remoteUrl : TODO_CONN_STR,
+    localUrl: TODO_CONN_STR
 };
